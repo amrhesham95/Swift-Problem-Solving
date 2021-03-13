53943c84dcae3fb7532238a9f6c0e994c52fccab
Amr Hesham <amrhesham9595@gmail.com>
2021-03-13 21:19:27 +0200

    Adding First Problem 	- Check if word has only unique charactersr, and returns true if the string has only unique letters

--- ProblemSolving/ProblemSolving/StringsSolver.swift
+++ ProblemSolving/ProblemSolving/StringsSolver.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+// MARK: - StringsSolver
+//
+class StringsSolver {
+  
+  /// Check if word has only unique charactersr, and returns true if the string has only unique letters
+  ///
+  /// - Parameters:
+  ///   - word: word to check it's characaters
+  func hasOnlyUniqueCharacters(word: String) -> Bool {
+    let wordAsSet = Set(word)
+    return wordAsSet.count == word.count
+  }
+}
