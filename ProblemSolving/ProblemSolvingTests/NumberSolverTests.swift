cf354e90400e5df63c141a32025a2e552ddcb828
Amr Hesham <amrhesham9595@gmail.com>
2021-03-19 18:49:53 +0200

    renaming binarGap tests

--- ProblemSolving/ProblemSolvingTests/NumberSolverTests.swift
+++ ProblemSolving/ProblemSolvingTests/NumberSolverTests.swift
@@ -29,38 +29,45 @@ class NumberSolverTests: XCTestCase {
     super.tearDown()
   }
     
-  // MARK: - Are the letters unique?
+  // MARK: - Binary Gap
   
   
-  func testNumberSolver_when9WasEntered_returnsLargestGapOfTwo() {
+  func testNumberSolver_whenBinaryGapFor9WasCalled_returnsLargestGapOfTwo() {
     let result = sut.binaryGapFor(9)
     
     XCTAssertEqual(result, 2, "number 9 has binary representation 1001 and contains a binary gap of length 2")
   }
   
-  func testNumberSolver_when529WasEntered_returnsLargestGapOfFour() {
+  func testNumberSolver_whenBinaryGapFor529WasCalled_returnsLargestGapOfFour() {
     let result = sut.binaryGapFor(529)
     
     XCTAssertEqual(result, 4, "number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3")
   }
   
-  func testNumberSolver_when20WasEntered_returnsLargestGapOfOne() {
+  func testNumberSolver_whenBinaryGapFor20WasCalled_returnsLargestGapOfOne() {
     let result = sut.binaryGapFor(20)
     
     XCTAssertEqual(result, 1, "umber 20 has binary representation 10100 and contains one binary gap of length 1")
   }
   
-  func testNumberSolver_when15WasEntered_returnsLargestGapOfZero() {
+  func testNumberSolver_whenBinaryGapFor15WasCalled_returnsLargestGapOfZero() {
     let result = sut.binaryGapFor(15)
     
     XCTAssertEqual(result, 0, "The number 15 has binary representation 1111 and has no binary gaps.")
   }
 
-  func testNumberSolver_when32WasEntered_returnsLargestGapOfZero() {
+  func testNumberSolver_whenBinaryGapFor32WasEntered_returnsLargestGapOfZero() {
     let result = sut.binaryGapFor(32)
     
     XCTAssertEqual(result, 0, "The number 32 has binary representation 100000 and has no binary gaps")
   }
 
+  // MARK: - CyclicRotation
+
+//  func testNumberSolver_whenBinaryGapFor32WasCalled_returnsLargestGapOfZero() {
+//    let result = sut.binaryGapFor(32)
+//
+//    XCTAssertEqual(result, 0, "The number 32 has binary representation 100000 and has no binary gaps")
+//  }
 
 }
