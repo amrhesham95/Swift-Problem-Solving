dbc1f0664ebe114ccf1bd42c03950325880b8311
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-11-30 11:33:24 +0200

    Challenge 20: Number is prime

---
+++ ProblemSolving/ProblemSolvingTests/NumberIsPrime.swift
@@ -0,0 +1,51 @@
+//
+//  NumberIsPrime.swift
+//  ProblemSolvingTests
+//
+//  Created by Amr Hesham on 30/11/2021.
+//
+
+import XCTest
+
+class NumberIsPrime: XCTestCase {
+    
+    /// a function that accepts an integer as its parameter and returns true if the number is prime.
+    /// - Parameters:
+    ///     - number: to check if it's prime or not
+    func isPrime(number: Int) -> Bool {
+        guard number > 1 else { return false }
+        for divisor in 2...Int(ceil(sqrt(Double(number)))) {
+            if (number % divisor)  == 0 {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func test_isPrime_WhenNumberIs11ShouldReturnTrue() {
+        XCTAssertTrue(isPrime(number: 11))
+    }
+    
+    func test_isPrime_WhenNumberIs13ShouldReturnTrue() {
+        XCTAssertTrue(isPrime(number: 13))
+    }
+    
+    func test_isPrime_WhenNumberIs4ShouldReturnFalse() {
+        XCTAssertFalse(isPrime(number: 4))
+    }
+    
+    func test_isPrime_WhenNumberIs9ShouldReturnFalse() {
+        XCTAssertFalse(isPrime(number: 9))
+    }
+    
+    func test_isPrime_WhenNumberIs16777259ShouldReturnTrue() {
+        XCTAssertTrue(isPrime(number: 16777259))
+    }
+    
+//    func test_isPrimeNormal_performance() {
+//        measure {
+//            isPrime(number: 16777259)
+//        }
+//    }
+
+}
